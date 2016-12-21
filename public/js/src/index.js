b025e002bf64123387332c71f6e513590c8773a5
Hanna Senkevich <hanna.senkevich@gmail.com>
2016-12-21 18:00:55 +0100

    Logic for adding emails to the members list + email validation

--- public/js/src/index.js
+++ public/js/src/index.js
@@ -1,7 +1,8 @@
 define(['jquery', 'jquery-ui', 'src/proposals'], function($, ui, proposals) {
-    $(document).ready(function() {
-        setProposal('template-0');
+    // eslint-disable-next-line no-useless-escape
+    var emailValidator = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+    function bindEvents() {
         $('.js-template-options').on('click', '.js-template-option', function(event) {
             var $selectedOption = $(event.target);
             var templateName = $selectedOption.data('template');
@@ -36,34 +37,73 @@ define(['jquery', 'jquery-ui', 'src/proposals'], function($, ui, proposals) {
             $autocompleteTarget.text(update);
         });
 
-        function setProposal(templateName) {
-            var proposal = proposals.find(function(element) {
-                return element.name === templateName;
-            });
-            $('.proposal-text').val(proposal.text);
-        }
+        $('.js-add-member-btn').on('click', function() {
+            addMember();
+        });
 
-        function switchStep($current, $next) {
-            $current.hide('slide', { direction: 'left' }, 'slow');
-            $next.removeClass('hidden');
-            $next.css({ display: 'none' });
-            $('.proposal-creator').css({ height: $next.height() });
-            $next.show('slide', { direction: 'right' }, 'slow');
-        }
-
-        function setDataForNextStep(step) {
-            switch (step) {
-                case 'js-answer-options-edit' :
-                    setCustomisedProposal();
-                    break;
-                default:
-                    break;
+        $('.js-member-input').on('keyup', function(e) {
+            $('.js-email-validation-error').addClass('invisible');
+            if (e.keyCode === 13) {
+                addMember();
             }
-        }
+        });
+    }
 
-        function setCustomisedProposal() {
-            var proposalText = $('.js-template-selection').find('.js-proposal-textarea').val();
-            $('.js-answer-options-edit').find('.js-proposal-textarea').html(proposalText);
+    function init() {
+        setProposal('template-0');
+        bindEvents();
+    }
+
+    function setProposal(templateName) {
+        var proposal = proposals.find(function(element) {
+            return element.name === templateName;
+        });
+        $('.proposal-text').val(proposal.text);
+    }
+
+    function switchStep($current, $next) {
+        $current.hide('slide', { direction: 'left' }, 'slow');
+        $next.removeClass('hidden');
+        $next.css({ display: 'none' });
+        $('.proposal-creator').css({ height: $next.height() });
+        $next.show('slide', { direction: 'right' }, 'slow');
+    }
+
+    function setDataForNextStep(step) {
+        switch (step) {
+            case 'js-answer-options-edit' :
+                setCustomisedProposal();
+                break;
+            default:
+                break;
         }
-    });
+    }
+
+    function setCustomisedProposal() {
+        var proposalText = $('.js-template-selection').find('.js-proposal-textarea').val();
+        $('.js-answer-options-edit').find('.js-proposal-textarea').html(proposalText);
+    }
+
+    function addMember() {
+        var $email = $('.js-member-input');
+        if (validateEmail($email.val())) {
+            renderMember($email.val());
+            $email.val('');
+        } else {
+            $('.js-email-validation-error').removeClass('invisible');
+        }
+    }
+
+    function renderMember(email) {
+        var member = $(document.createElement('span'));
+        member.addClass('label label-email');
+        member.html(email + ' <a><i class="icon-cross icon"></i></a>');
+        $('.js-members-list').append(member).append(' ');
+    }
+
+    function validateEmail(email) {
+        return emailValidator.test(email);
+    }
+
+    $(document).ready(init);
 });
